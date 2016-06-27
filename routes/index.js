ec84d02b7799db3533277d8092bac0880ace9b4a
Yen-chi Chen <mark86092@gmail.com>
2016-06-27 15:47:35 +0800

    integrate mongodb and save columns

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,12 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     var access_token = req.body.access_token;
+    var
+        company = req.body.company,
+        title = req.body.title,
+        worktime = req.body.worktime,
+        salary = req.body.salary,
+        workyear = req.body.workyear;
 
     request.get({
         url: "https://graph.facebook.com/v2.6/me",
@@ -20,8 +27,29 @@ router.post('/', function(req, res, next) {
     }, function(error, response, body) {
         var content = JSON.parse(body);
 
-        res.header("Access-Control-Allow-Origin", "*");
-        res.send(content);
+        MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+            var collection = db.collection('workings');
+
+            var data = {
+                author: {
+                    id: content.id,
+                    name: content.name,
+                    type: 'facebook',
+                },
+                company: company,
+                title: title,
+                worktime: worktime,
+                salary: salary,
+                workyear: workyear,
+            };
+
+            collection.insert(data, function(err, result) {
+                db.close();
+
+                res.header("Access-Control-Allow-Origin", "*");
+                res.send(data);
+            });
+        });
     });
 
 });
