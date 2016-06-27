1ec4688a1db8b155f1c93ee244c02bc37d5f9c23
Yen-chi Chen <mark86092@gmail.com>
2016-06-27 14:51:45 +0800

    Receive token and auth it

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({});
+});
+
+router.post('/', function(req, res, next) {
+    var access_token = req.body.access_token;
+
+    request.get({
+        url: "https://graph.facebook.com/v2.6/me",
+        qs: {
+            access_token: access_token,
+            fields: "id,name",
+            format: "json",
+        }
+    }, function(error, response, body) {
+        var content = JSON.parse(body);
+
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(content);
+    });
+
 });
 
 module.exports = router;
