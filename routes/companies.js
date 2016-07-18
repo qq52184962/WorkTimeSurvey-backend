199f057bca41767389e801f669b4be139b697877
Yen-chi Chen <mark86092@gmail.com>
2016-07-18 21:23:36 +0800

    bug fix

--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var HttpError = require('./errors').HttpError;
 var lodash = require('lodash');
+var winston = require('winston');
 
 /*
  * GET /
