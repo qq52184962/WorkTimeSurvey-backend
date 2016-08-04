6bbab2c7fe48f514c5b345deb93c257b7e405273
Yen-chi Chen <mark86092@gmail.com>
2016-08-04 10:32:32 +0800

    Deploy the apidoc

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+npm install apidoc -g
+
+REV=`git describe --always`
+
 git clone git@github.com:goodjoblife/WorkTimeSurvey-backend.git doc -b gh-pages --depth 1
 
 apidoc -i routes/
@@ -7,5 +11,5 @@ apidoc -i routes/
 cd doc
 
 git add -A .
-git commit -m "apidoc"
-git push
+git commit -m "apidoc for $REV"
+git push origin gh-pages
