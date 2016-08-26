a5a6c1a04387fe50b1e2f6e12e633776062ea6ca
Yen-chi Chen <mark86092@gmail.com>
2016-08-26 11:23:28 +0800

    Add tests for CORS (#92)

---
+++ test/testGeneral.js
@@ -0,0 +1,34 @@
+const assert = require('chai').assert;
+const request = require('supertest');
+const app = require('../app');
+const MongoClient = require('mongodb').MongoClient;
+
+describe('CORS', function() {
+    const client_origins = [
+        'http://localhost:8080',
+        'http://localhost:8000',
+        'https://worktime.goodjob.life',
+    ];
+
+    for (let origin of client_origins) {
+        it(origin + ' is in cors list', function(done) {
+            request(app).get('/')
+                .set('origin', origin)
+                .expect(404)
+                .expect(function(res) {
+                    assert.propertyVal(res.header, 'access-control-allow-origin', origin);
+                })
+                .end(done);
+        });
+    }
+
+    it('reject other origin', function(done) {
+        request(app).get('/')
+            .set('origin', 'http://www.google.com.tw')
+            .expect(404)
+            .expect(function(res) {
+                assert.notProperty(res.header, 'access-control-allow-origin');
+            })
+            .end(done);
+    });
+});
