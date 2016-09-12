0dec7518c6259d827284eb7593e5b6fa69836096
Yen-chi Chen <mark86092@gmail.com>
2016-09-12 21:33:50 +0800

    Minor fix for db and travis (#108)
    * refactor db testing
    * coding style (indent and ;)
    * ability to handle err callback (by return Promise)
    * fix the indent of .travis.yml
    * change mocha timeout value for travis env

---
+++ test/testMongoDB.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient
+const assert = require('chai').assert;
+
+describe('MongoDB version', function() {
+    it('should be at least 3.x.x', function() {
+        return MongoClient.connect(process.env.MONGODB_URI)
+            .then(db => db.admin().serverStatus())
+            .then(info => {
+                let v = parseInt(info.version.split('.')[0]);
+                assert.isAtLeast(v, 3, 'current version is ' + info.version);
+            });
+    });
+});
