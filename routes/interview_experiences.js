3bbd749cd85b9bc0d69a77ea6c3cbf861040b327
Yen-chi Chen <mark86092@gmail.com>
2017-03-29 15:26:36 +0800

    初版：上傳面試經驗 API (#202)
    * finish sample API of POST /interview_experiences
    
    * remove facebook auth
    
    * change path
    
    * finish post API for interview experiences
    
    * finish API test for interview experiences
    
    * fix coding style
    
    * change path

--- routes/interview_experiences.js
+++ routes/interview_experiences.js
@@ -1,8 +1,226 @@
 const express = require('express');
 const router = express.Router();
+const HttpError = require('../libs/errors').HttpError;
+const winston = require('winston');
+const helper = require('./workings_helper');
 
 router.post('/', function(req, res, next) {
-    //
-});
+    const data_fields = [
+        // Required
+        "author_id",
+        "author_type",
+        "area",
+        "job_title",
+        "interview_time_year",
+        "interview_time_month",
+        "interview_result",
+        "overall_rating",
+        "title",
+        // Optional
+        "sections",
+        "interview_qas",
+        "experience_in_year",
+        "education",
+        "salary_type",
+        "salary_amount",
+    ];
+
+    req.custom = {};
+    req.custom.experience = {
+        author: {},
+        company: {},
+        created_at: new Date(),
+        type: "interview",
+    };
+
+    collectExperienceData(req, data_fields).then(next, next);
+}, function(req, res, next) {
+    validation(req).then(next, next);
+}, main);
+
+
+function collectExperienceData(req, fields) {
+    const experience = req.custom.experience;
+
+    // pick fields only
+    // make sure the field is string
+    fields.forEach((field) => {
+        if (checkBodyField(req, field)) {
+            experience[field] = req.body[field];
+        }
+    });
+
+    if (checkBodyField(req, "company_id")) {
+        experience.company.id = req.body.company_id;
+    } else if (checkBodyField(req, "company")) {
+        experience.company.query = req.body.company;
+    }
+
+    return Promise.resolve();
+}
+
+function validation(req) {
+    const experience = req.custom.experience;
+
+    try {
+        validateCommonData(experience);
+        if (experience.type === "interview") {
+            validateInterviewData(experience);
+        } else if (experience.type === "work") {
+            validateWorkData(experience);
+        }
+    } catch (err) {
+        winston.info("validating fail", {ip: req.ip, ips: req.ips});
+
+        return Promise.reject(err);
+    }
+
+    return Promise.resolve();
+}
+
+function validateCommonData(data) {
+    /*
+    if (! data.author) {
+        throw new HttpError("使用者沒登入嗎？", 500);
+    }
+    */
+    if (! data.company.id && ! data.company.query) {
+        throw new HttpError("公司/單位名稱要填喔！", 422);
+    }
+    if (! data.area) {
+        throw new HttpError("地區要填喔！", 422);
+    }
+    if (! data.job_title) {
+        throw new HttpError("職稱要填喔！", 422);
+    }
+    if (! data.title) {
+        throw new HttpError("標題要寫喔！", 422);
+    }
+}
+
+function validateInterviewData(data) {
+
+    // Required
+    if (! data.interview_time_year) {
+        throw new HttpError("面試年份要填喔！", 422);
+    } else if (! data.interview_time_month) {
+        throw new HttpError("面試月份要填喔！", 422);
+    } else {
+        data.interview_time = {
+            year: parseInt(data.interview_time_year),
+            month: parseInt(data.interview_time_month),
+        };
+        delete data.interview_time_year;
+        delete data.interview_time_month;
+
+        const now = new Date();
+        if (isNaN(data.interview_time.year)) {
+            throw new HttpError('面試年份需為數字', 422);
+        } else if (data.interview_time.year <= now.getFullYear() - 10) {
+            throw new HttpError('面試年份需在10年內', 422);
+        }
+        if (isNaN(data.interview_time.month)) {
+            throw new HttpError('面試月份需為數字', 422);
+        } else if (data.interview_time.month < 1 || data.interview_time.month > 12) {
+            throw new HttpError('面試月份需在1~12月', 422);
+        }
+        if ((data.interview_time.year === now.getFullYear() && data.interview_time.month > (now.getMonth() + 1)) ||
+            data.interview_time.year > now.getFullYear()) {
+            throw new HttpError('面試月份不可能比現在時間晚', 422);
+        }
+    }
+
+    if (! data.interview_result) {
+        throw new HttpError("面試結果要填喔！", 422);
+    }
+
+    if (! data.overall_rating) {
+        throw new HttpError("這次面試你給幾分？", 422);
+    } else if (["1", "2", "3", "4", "5"].indexOf(data.overall_rating) === -1) {
+        throw new HttpError('面試分數有誤', 422);
+    }
+
+    // Optional
+    if (data.experience_in_year) {
+        data.experience_in_year = parseInt(data.experience_in_year);
+        if (isNaN(data.experience_in_year)) {
+            throw new HttpError('相關職務工作經驗需為數字', 422);
+        } else if (data.experience_in_year < 0 || data.experience_in_year > 50) {
+            throw new HttpError('相關職務工作經驗需大於等於0，小於等於50', 422);
+        }
+    }
+
+    if (data.education) {
+        // todo
+    }
+
+    if (data.salary_amount && data.salary_type) {
+        data.salary = {
+            amount: parseInt(data.salary_amount),
+            type: data.salary_type,
+        };
+
+        delete data.salary_amount;
+        delete data.salary_type;
+
+        if (["year", "month", "day", "hour"].indexOf(data.salary.type) === -1) {
+            throw new HttpError('薪資種類需為年薪/月薪/日薪/時薪', 422);
+        }
+        if (isNaN(data.salary.amount)) {
+            throw new HttpError('薪資需為數字', 422);
+        } else if (data.salary.amount < 0) {
+            throw new HttpError('薪資不小於0', 422);
+        }
+    }
+}
+
+function validateWorkData(data) {
+    //todo
+}
+
+function checkBodyField(req, field) {
+    if (req.body[field] && (typeof req.body[field] === "string") && req.body[field] !== "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function main(req, res, next) {
+    const experience = req.custom.experience;
+    const response_data = {
+        experience: experience,
+    };
+    const collection = req.db.collection("experiences");
+
+    /*
+     * Normalize the data
+     */
+    experience.job_title = experience.job_title.toUpperCase();
+
+    /*
+     *  這邊處理需要呼叫async函數的部份
+     */
+    /*
+     * 如果使用者有給定 company id，將 company name 補成查詢到的公司
+     *
+     * 如果使用者是給定 company name，如果只找到一間公司，才補上 id
+     *
+     * 其他情況看 issue #7
+     */
+    helper.normalizeCompany(req.db, experience.company.id, experience.company.query).then(company => {
+        experience.company = company;
+
+        return collection.insert(experience);
+    }).then(() => {
+        winston.info("experiences insert data success", {id: experience._id, ip: req.ip, ips: req.ips});
+
+        res.send(response_data);
+    }).catch(err => {
+        winston.info("experiences insert data fail", {id: experience._id, ip: req.ip, ips: req.ips, err: err});
+
+        next(err);
+    });
+}
 
 module.exports = router;
---
+++ test/testExperiencesPost.js
@@ -0,0 +1,309 @@
+//const chai = require('chai');
+//const assert = chai.assert;
+const request = require('supertest');
+const app = require('../app');
+const MongoClient = require('mongodb').MongoClient;
+
+describe('experiences 面試和工作經驗資訊', function() {
+    let db = undefined;
+
+    before('DB: Setup', function() {
+        return MongoClient.connect(process.env.MONGODB_URI).then(function(_db) {
+            db = _db;
+        });
+    });
+
+
+    describe('POST /interview_experiences', function() {
+        before('Seed companies', function() {
+            return db.collection('companies').insertMany([
+                {
+                    id: '00000001',
+                    name: 'GOODJOB',
+                },
+                {
+                    id: '00000002',
+                    name: 'GOODJOBGREAT',
+                },
+                {
+                    id: '00000003',
+                    name: 'GOODJOBGREAT',
+                },
+            ]);
+        });
+
+        describe('generate payload', function() {
+            it('generateInterViewExperiencePayload', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload())
+                    .expect(200)
+                    .end(done);
+            });
+        });
+
+        describe('Common Data Validation Part', function() {
+            it('company or company_id is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        company: -1,
+                        company_id: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it('area is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        area: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it('job_title is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        job_title: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it('title is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        title: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+        });
+
+        describe('Interview Validation Part', function() {
+            it('interview_time_year is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        interview_time_year: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it('interview_time_month is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        interview_time_month: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            describe('interview_time should be reasonable', function() {
+                it('interview_time_year sould be number', function(done) {
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            interview_time_year: "a",
+                        }))
+                        .expect(422)
+                        .end(done);
+                });
+
+                it('interview_time_month sould be number', function(done) {
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            interview_time_year: "a",
+                        }))
+                        .expect(422)
+                        .end(done);
+                });
+
+                it('interview_time_year <= this year', function(done) {
+                    let nextYear = new Date();
+                    nextYear.setFullYear(nextYear.getFullYear() + 1);
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            interview_time_year: nextYear.getFullYear().toString(),
+                        }))
+                        .expect(422)
+                        .end(done);
+                });
+
+                it('interview_time_year > this year - 10', function(done) {
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            interview_time_year: ((new Date()).getFullYear() - 10).toString(),
+                        }))
+                        .expect(422)
+                        .end(done);
+                });
+
+                it('interview_time_month should be 1~12', function(done) {
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            interview_time_month: "13",
+                        }))
+                        .expect(422)
+                        .end(done);
+                });
+
+                it('interview_time <= now', function(done) {
+                    let now = new Date();
+
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            interview_time_year: now.getFullYear().toString(),
+                            interview_time_month: (now.getMonth() + 2).toString(),
+                        }))
+                        .expect(422)
+                        .end(done);
+                });
+            });
+
+            it('interview_result is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        interview_result: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it.skip('interview_result could not be others', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        interview_result: "invalid",
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            for (let result of [""]) {
+                it.skip(`interview_result should be ${result}`, function(done) {
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            interview_result: result,
+                        }))
+                        .expect(200)
+                        .expect(function(res) {
+                            // todo
+                        })
+                        .end(done);
+                });
+            }
+
+            it('overall_rating is required', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        overall_rating: -1,
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it('overall_rating should be 1~5', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        overall_rating: "6",
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it('experience_in_year should not be a valid number', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        experience_in_year: "test",
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            it('experience_in_year should be 0~50', function(done) {
+                request(app).post('/interview_experiences')
+                    .send(generateInterviewExperiencePayload({
+                        experience_in_year: "51",
+                    }))
+                    .expect(422)
+                    .end(done);
+            });
+
+            for (let input of [""]) {
+                it.skip(`education should be ${input}`, function(done) {
+                    request(app).post('/interview_experiences')
+                        .send(generateInterviewExperiencePayload({
+                            education: input,
+                        }))
+                        .expect(200)
+                        .expect(function(res) {
+                            // todo
+                        })
+                        .end(done);
+                });
+            }
+        });
+
+        after('DB: 清除 experiences', function() {
+            return db.collection('experiences').remove({});
+        });
+
+        after('DB: 清除 companies', function() {
+            return db.collection('companies').remove({});
+        });
+    });
+});
+
+function generateInterviewExperiencePayload(opt) {
+    opt = opt || {};
+    const valid = {
+        author_type: "facebook",
+        author_id: "id123",
+        company_id: '00000001',
+        area: "Taipei",
+        job_title: 'job_title_example',
+        title: "title_example",
+        sections: [
+            {
+                subtitle: "subtitle1",
+                content: "content1",
+            },
+        ],
+        experience_in_year: '10',
+        education: "BS",
+        // Interview Experience related
+        interview_time_year: "2017",
+        interview_time_month: "3",
+        interview_qas: [
+            {
+                question: "qas1",
+                answer: "ans1",
+            },
+        ],
+        interview_result: "up",
+        salary_type: 'year',
+        salary_amount: '10000',
+        overall_rating: "5",
+    };
+
+    var payload = {};
+    for (let key in valid) {
+        if (opt[key]) {
+            if (opt[key] === -1) {
+                continue;
+            } else {
+                payload[key] = opt[key];
+            }
+        } else {
+            payload[key] = valid[key];
+        }
+    }
+    for (let key in opt) {
+        if (opt[key] === -1) {
+            continue;
+        } else {
+            payload[key] = opt[key];
+        }
+    }
+
+    return payload;
+}
