2725ba8d6b5c0ea19a8b62053c35576ae013d2ce
Yen-chi Chen <mark86092@gmail.com>
2017-08-08 10:19:03 -0500

    Add dev server support (#384)

--- .circleci/deploy-docker.sh
+++ .circleci/deploy-docker.sh
@@ -6,6 +6,16 @@ REPO="059402281999.dkr.ecr.ap-northeast-1.amazonaws.com/goodjob/api-server"
 
 ./.circleci/prepare-docker-login.sh || exit 1
 docker tag "${REPO}:latest" "${REPO}:${CIRCLE_SHA1}" || exit 1
-docker tag "${REPO}:latest" "${REPO}:stage" || exit 1
 docker push "${REPO}:${CIRCLE_SHA1}" || exit 1
-docker push "${REPO}:stage" || exit 1
+
+# stage image should be replaced on master branch
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+    docker tag "${REPO}:latest" "${REPO}:stage" || exit 1
+    docker push "${REPO}:stage" || exit 1
+fi
+
+# dev image should be replaced on dev branch
+if [ "${CIRCLE_BRANCH}" == "dev" ]; then
+    docker tag "${REPO}:latest" "${REPO}:dev" || exit 1
+    docker push "${REPO}:dev" || exit 1
+fi
