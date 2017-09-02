847612e327111ca55eee9c4bd0b807e4f167b58a
Yen-chi Chen <mark86092@gmail.com>
2017-09-02 13:05:52 -0500

    Seperate routes declaration (#445)

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+
+const router = express.Router();
+
+const corsOption = {
+    origin: [
+        new RegExp(".*://www.104.com.tw"),
+        new RegExp(".*://104.com.tw"),
+        new RegExp("http://www.1111.com.tw"),
+        new RegExp("http://www.518.com.tw"),
+        new RegExp(".*://www.yes123.com.tw"),
+    ],
+};
+
+// please sort in alphabetical order
+router.use('/clairvoyance/search', cors(corsOption), require('./clairvoyance/search'));
+router.use('/companies', require('./companies'));
+router.use('/company_keywords', require('./company_keywords'));
+router.use('/experiences', require('./experiences'));
+router.use('/interview_experiences', require('./interview_experiences'));
+router.use('/jobs', require('./jobs'));
+router.use('/job_title_keywords', require('./job_title_keywords'));
+router.use('/me', require('./me'));
+router.use('/replies', require('./replies'));
+router.use('/workings', require('./workings'));
+router.use('/work_experiences', require('./work_experiences'));
+
+module.exports = router;
