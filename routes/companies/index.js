728c1bc37ea564a6edd989098cab7efc191ebe52
Yen-chi Chen <mark86092@gmail.com>
2018-04-15 00:35:21 +0800

    用 ramda 改寫, 並改成 async/await

--- routes/companies/index.js
+++ routes/companies/index.js
@@ -1,19 +1,26 @@
 const express = require("express");
-const HttpError = require("../../libs/errors").HttpError;
 const lodash = require("lodash");
+const R = require("ramda");
+const { HttpError } = require("../../libs/errors");
 const CompanyModel = require("../../models/company_model");
-const getCompanyName = require("../company_helper").getCompanyName;
+const { getCompanyName } = require("../company_helper");
+const wrap = require("../../libs/wrap");
+const { combineSelector } = require("../../view_models/helper");
 
 const router = express.Router();
 
-function _generateGetCompanyViewModel(companies) {
-    const result = companies.map(company => ({
-        id: company.id,
-        name: getCompanyName(company.name),
-        capital: company.capital,
-    }));
-    return result;
-}
+/**
+ * @param company
+ */
+const companyView = combineSelector([
+    R.pick(["id", "capital"]),
+    company => ({ name: getCompanyName(company.name) }),
+]);
+
+/**
+ * @param companies
+ */
+const searchView = R.map(companyView);
 
 /**
  * @api {get} /companies/search Search Company
@@ -23,45 +30,48 @@ function _generateGetCompanyViewModel(companies) {
  * @apiParam {Number} [page=0]
  * @apiSuccess {Object[]} . Companies
  */
-router.get("/search", (req, res, next) => {
-    const search = req.query.key || "";
-    const page = req.query.page || 0;
+router.get(
+    "/search",
+    wrap(async (req, res) => {
+        const search = req.query.key || "";
+        const page = req.query.page || 0;
 
-    if (search === "") {
-        next(new HttpError("key is required", 422));
-        return;
-    }
+        if (search === "") {
+            throw new HttpError("key is required", 422);
+            return;
+        }
 
-    const query = {
-        $or: [
-            {
-                name: new RegExp(
-                    `^${lodash.escapeRegExp(search.toUpperCase())}`
-                ),
-            },
-            { id: search },
-        ],
-    };
+        const query = {
+            $or: [
+                {
+                    name: new RegExp(
+                        `^${lodash.escapeRegExp(search.toUpperCase())}`
+                    ),
+                },
+                { id: search },
+            ],
+        };
 
-    const sort_by = {
-        capital: -1,
-        type: -1,
-        name: 1,
-        id: 1,
-    };
+        const sort_by = {
+            capital: -1,
+            type: -1,
+            name: 1,
+            id: 1,
+        };
 
-    const company_model = new CompanyModel(req.db);
-    const skip = 25 * page;
-    const limit = 25;
+        const company_model = new CompanyModel(req.db);
+        const skip = 25 * page;
+        const limit = 25;
 
-    company_model
-        .searchCompany(query, sort_by, skip, limit)
-        .then(results => {
-            res.send(_generateGetCompanyViewModel(results));
-        })
-        .catch(err => {
-            next(new HttpError("Internal Server Error", 500));
-        });
-});
+        const companies = await company_model.searchCompany(
+            query,
+            sort_by,
+            skip,
+            limit
+        );
+
+        res.send(searchView(companies));
+    })
+);
 
 module.exports = router;
