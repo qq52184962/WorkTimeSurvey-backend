d1a845e83207628dcdd0ccaf8e377a43a03657f5
Yen-chi Chen <mark86092@gmail.com>
2019-05-30 22:46:49 +0800

    Adjust legacy settings

--- test/config-env.js
+++ test/config-env.js
@@ -1,3 +1,4 @@
 process.env.CONTENTFUL_ACCESS_TOKEN = "FakeTokenYouWillFail";
 process.env.CONTENTFUL_SPACE = "rhotsuly6hr2";
 process.env.JWT_SECRET = "DontUseMe";
+process.env.VERIFY_EMAIL_JWT_SECRET = "DontUseMe";
